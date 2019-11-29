923b63135101f82213b9788c0e970ef1c5aa8a44
Viktor Varland <varl@vlv.io>
2019-11-29 17:08:16 +0100

    docs: restructure a bit to make room for e.g. android

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@ const { config } = require('@dhis2/cli-style')
 module.exports = {
     hooks: {
         ...config.husky.hooks,
-        'pre-commit': 'd2-style text check "**/*.{md,json,yml,yaml}" --staged',
+        'pre-commit': 'd2-style text check --pattern "**/*.{md,json,yml,yaml}" --staged',
     },
 }
